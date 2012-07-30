3d7c97fe96120bc1898150ef4c1916f4d449584d
Danial Pearce <git@tigris.id.au>
2012-07-30 14:29:25 +1000

    Use cookbook file overrides to find the ubuntu template

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,16 @@ if platform?("ubuntu")
     group "root"
     mode 0644
   end
+
+  # Working init script, fix for: https://bugs.launchpad.net/ubuntu/+source/monit/+bug/993381
+  if node.platform_version.to_f == 12.04
+    cookbook_file "/etc/init.d/monit" do
+      source "monit.init.sh"
+      owner "root"
+      group "root"
+      mode 0755
+    end
+  end
 end
 
 service "monit" do
